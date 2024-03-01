8b9c82b969380851b5dcdfd691e25ffa22a739c7
Jared-Soehner <77736345+Jared-Soehner@users.noreply.github.com>
2024-02-29 20:10:16 -0400

    Changed a single line to remove the placement issue when doing x(0,0)-> o(1,0) -> x(1,1) placement issue

--- src/Game.java
+++ src/Game.java
@@ -86,7 +86,7 @@ class Game implements Disposer {
     if (player == Player.HOST) {
       this.game[x][y] = State.X;
     } else {
-      this.game[y][y] = State.O;
+      this.game[x][y] = State.O;
     }
 
     Game.log.info(player.toString() + " played " + this.game[x][y].toString() + " at " + x + ", " + y);
